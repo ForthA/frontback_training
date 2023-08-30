5887d0b14759fde692e7f79f83cd072bd9bf7233
AlexanderNP <enin02@mail.ru>
2023-08-30 22:26:21 +0500

    added page: added

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,20 @@
-new Splide( '#list__slider', {
-  type: 'slide',
-  perPage: 2,
-  perMove: 2,
-  rewind: true,
-  speed: 1000,
-  pagination: false,
-} ).mount();
+if (window.location.toString().indexOf('added.html') > 0) {
+  const fileInput = document.getElementById('imageUpload');
+  const preview = document.getElementById('preview');
+
+  fileInput.addEventListener('change', function () {
+    const file = this.files[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      const img = document.createElement('img');
+      img.src = url;
+      img.onload = function () {
+        if (this.width === 250 && this.height === 250) {
+          preview.appendChild(img);
+        } else {
+          alert('Изображение должно быть размером 500x500px');
+        }
+      };
+    }
+  });
+}
